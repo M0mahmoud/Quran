7774696ec2abb5bf81b2a4901c19d4e7e63c5f87
M0mahmoud <houdmohamed85@gmail.com>
2023-02-11 10:31:11 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Container } from "react-bootstrap";
+import { Outlet } from "react-router-dom";
+
+function App() {
+  return (
+    <Container>
+      <Outlet />
+    </Container>
+  );
+}
+
+export default App;
